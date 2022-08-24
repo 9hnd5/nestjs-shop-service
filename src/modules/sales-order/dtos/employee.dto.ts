0ba984c20c0d67a20d892ef91e844462edd12697
ngocchi <ngocchi1115@gmail.com>
2022-08-24 14:52:00 +0700

    update api paging/by-salesman

---
+++ src/modules/sales-order/dtos/employee.dto.ts
@@ -0,0 +1,27 @@
+export class Employee {
+    id: number;
+    code: string;
+    childrenNumber?: number;
+    address?: string;
+    departmentId: number;
+    dob?: Date;
+    email?: string;
+    emergencyPerson?: string;
+    emergencyPhone?: string;
+    firstName: string;
+    gender?: string;
+    identityNumber?: string;
+    image?: string;
+    isActive: boolean;
+    lastName: string;
+    maritalStatusId?: number;
+    middleName?: string;
+    phone?: string;
+    positionId?: number;
+    salesOrganizationId: number;
+    salesTerritoryId: number;
+    startDate?: Date;
+    terminationDate?: Date;
+    titleId?: number;
+    userId?: number;
+}
